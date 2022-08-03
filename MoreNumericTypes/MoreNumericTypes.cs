a8162358cfa899b2ba08d8d4a6ba875ada6a4364
ysa-v <ysa.vandenberg@gmail.com>
2022-08-03 14:14:37 -0500

    Finished all exercises

---
+++ MoreNumericTypes/MoreNumericTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MoreNumericTypes
+{
+    class MoreNumericTypes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many miles have you driven?");
+            string milesInput = Console.ReadLine();
+            Console.WriteLine("How much gas (in gallons) have you used?");
+            string gasInput = Console.ReadLine();
+
+            double miles = double.Parse(milesInput);
+            double gas = double.Parse(gasInput);
+
+            double mpg = miles / gas;
+
+            Console.WriteLine("Your mpg is " + mpg.ToString() + ".");
+        }
+    }
+}
+
---
+++ NumericTypes/NumericTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumericTypes
+{
+    class NumericTypes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the height?");
+            string heightInput = Console.ReadLine();
+            Console.WriteLine("What is the width?");
+            string widthInput = Console.ReadLine();
+
+            double height = double.Parse(heightInput);
+            double width = double.Parse(widthInput);
+
+            double area = height * width;
+
+            Console.WriteLine("The area of the rectangle is " + area.ToString() + ".");
+        }
+    }
+}
+
