53d4e96c7830e1af5d58ec50d2ab3c2d98351655
ysa-v <ysa.vandenberg@gmail.com>
2022-08-03 13:05:15 -0500

    Name change for clarity

---
+++ ExercisesDataTypes/InputOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExercisesDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
+
